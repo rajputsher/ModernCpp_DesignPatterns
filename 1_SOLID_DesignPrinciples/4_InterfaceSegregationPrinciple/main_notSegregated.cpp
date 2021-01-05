c413724ad52fdbd5c2dab522b19040b7df8c815b
rajputsher <shersingh72@gmail.com>
2021-01-05 14:38:32 +0100

    Interface Segregation Principle

---
+++ 1_SOLID_DesignPrinciples/4_InterfaceSegregationPrinciple/main_notSegregated.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <boost/lexical_cast.hpp>
+
+using namespace std;
+using namespace boost;
+
+struct Document;
+struct IMachine
+{
+    virtual void print(Document& doc) = 0;
+    virtual void scan(Document& doc) = 0;
+    virtual void fax(Document& doc)= 0;
+};
+
+//If I need a MultiFinctionPrinter which can scan, print and fax, I have a class from which I can derive
+struct MFP: IMachine
+{
+    void print(Document &doc) override
+    {
+
+    }
+    void scan(Document &doc) override
+    {
+
+    }
+    void fax(Document &doc) override
+    {
+        
+    }
+};
+
+//Now if I need another machine that I only need for scan, we have an issue
+//What do we do with the print() and fax()?
+//we can leave than blank or write exceptions for using
+//however we are giving a wrong machine to the client by giving unnecessary interfaces(APIS)
+//Hence we need to Segregate the interfaces.(Check another main.cpp file)
+struct Scanner: IMachine
+{
+    void print(Document &doc) override
+    {
+
+    }
+    void scan(Document &doc) override
+    {
+        //OK
+    }
+    void fax(Document &doc) override
+    {
+        
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
